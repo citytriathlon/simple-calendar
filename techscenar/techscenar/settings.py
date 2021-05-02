fed5ecbc54dd89d468fecd764352f533932e793f
Jan Novak <jan.novak@infor.com>
2021-05-02 15:49:43 +0200

    10.180.0.48

--- techscenar/techscenar/settings.py
+++ techscenar/techscenar/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@lh2bacj=hs5v$%j+ybh#9!zqzv3unjl#f%4lj-9e5$-_fd56h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["10.180.0.48"]
 
 
 # Application definition
