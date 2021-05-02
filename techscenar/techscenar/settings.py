0926c28f026a392587c1c822281a0bea6aa0ea5e
Jan Novak <jan.novak@infor.com>
2021-05-02 22:02:38 +0200

    static

--- techscenar/techscenar/settings.py
+++ techscenar/techscenar/settings.py
@@ -132,6 +132,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
