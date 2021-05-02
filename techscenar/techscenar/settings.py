9a9759d56052c81f67970e0650aac706fa1bea7c
Jan Novak <jan.novak@infor.com>
2021-05-02 15:47:58 +0200

    adding all hosts

--- techscenar/techscenar/settings.py
+++ techscenar/techscenar/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@lh2bacj=hs5v$%j+ybh#9!zqzv3unjl#f%4lj-9e5$-_fd56h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
